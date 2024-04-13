e88fbaabe4bde7643480f8bdbbd8e44b99c58d01
arinming <jyandysys1@naver.com>
2024-04-13 22:48:28 +0900

    [Feat] 버튼 바운싱 효과 추가 (#3)

--- lib/presentation/login/view/login_screen.dart
+++ lib/presentation/login/view/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:naeng_meh_chu/core/app_bar/primary_app_bar.dart';
+import 'package:naeng_meh_chu/core/base/bouncing.dart';
 import 'package:naeng_meh_chu/core/theme/naeng_meh_chu_theme_color.dart';
 import 'package:naeng_meh_chu/core/theme/naeng_meh_chu_theme_text_style.dart';
 
@@ -26,35 +27,37 @@ class LoginScreen extends StatelessWidget {
               '냉메추',
               style: NaengMehChuThemeTextStyle.pinkBold24,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: NaengMehChuThemeColor.naverColor
-                ),
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icon/ic_naver.svg',
-                        width: 36,
-                        height: 36,
-                        colorFilter: const ColorFilter.mode(
-                            NaengMehChuThemeColor.white, BlendMode.srcIn),
-                      ),
-                      const Expanded(
-                        child: Center(
-                          child: Text(
-                            '네이버로 시작하기',
-                            style: NaengMehChuThemeTextStyle.blackMedium15,
+            Bouncing(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: NaengMehChuThemeColor.naverColor
+                  ),
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icon/ic_naver.svg',
+                          width: 36,
+                          height: 36,
+                          colorFilter: const ColorFilter.mode(
+                              NaengMehChuThemeColor.white, BlendMode.srcIn),
+                        ),
+                        const Expanded(
+                          child: Center(
+                            child: Text(
+                              '네이버로 시작하기',
+                              style: NaengMehChuThemeTextStyle.blackMedium15,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
