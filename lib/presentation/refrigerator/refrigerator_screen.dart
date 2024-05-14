dbe0cb66b76e89c4323213a58bd333146592322b
arinming <jyandysys1@naver.com>
2024-05-14 14:12:59 +0900

    [Feat] bottom navigation 추가(#10)

---
+++ lib/presentation/refrigerator/refrigerator_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RefrigeratorScreen extends ConsumerWidget {
+  const RefrigeratorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
