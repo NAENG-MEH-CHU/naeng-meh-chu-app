c31d0310b43e436b7f8e5e3b506a9ec6d090ecce
arinming <jyandysys1@naver.com>
2024-06-05 22:56:18 +0900

    [Feat] 전체 레시피 조회 페이징 추가 (#18)

--- lib/presentation/recipe/recipe_screen.dart
+++ lib/presentation/recipe/recipe_screen.dart
@@ -2,16 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:naeng_meh_chu/data/model/recipe_data_response.dart';
 import 'package:naeng_meh_chu/presentation/recipe/view/recipe_card.dart';
-import 'package:naeng_meh_chu/presentation/recipe/view/search_container.dart';
 import 'package:naeng_meh_chu/presentation/recipe/view_model/all_recipe_notifier.dart';
 
 import '../../core/app_bar/main_app_bar.dart';
 
-class RecipeScreen extends ConsumerWidget {
+class RecipeScreen extends ConsumerStatefulWidget {
   const RecipeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RecipeScreen> createState() => _RecipeScreenState();
+}
+
+class _RecipeScreenState extends ConsumerState<RecipeScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      ref.read(recipeDataResponseProvider.notifier).fetchMoreRecipes();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final AsyncValue<List<RecipeDataResponse>> recipe =
         ref.watch(recipeDataResponseProvider);
 
@@ -20,26 +46,24 @@ class RecipeScreen extends ConsumerWidget {
         title: '레시피',
       ),
       body: recipe.when(
-        data: (recipes) => SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                const SearchContainer(
-                  message: '오늘의 레시피를 검색해 보세요',
-                ),
-                const SizedBox(
-                  height: 32.0,
-                ),
-                ...recipes
-                    .map((recipe) => RecipeCard(
-                          title: recipe.name,
-                          description: '',
-                          thumbnail: recipe.thumbnail,
-                        )),
-              ],
-            ),
-          ),
+        data: (recipes) => ListView.builder(
+          controller: _scrollController,
+          itemCount: recipes.length + 1,
+          itemBuilder: (context, index) {
+            if (index < recipes.length) {
+              final recipe = recipes[index];
+              return RecipeCard(
+                title: recipe.name,
+                description: '',
+                thumbnail: recipe.thumbnail,
+              );
+            } else {
+              return const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Center(child: CircularProgressIndicator()),
+              );
+            }
+          },
         ),
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (err, stack) => Center(child: Text('Error: $err')),
--- lib/presentation/recipe/view_model/all_recipe_notifier.dart
+++ lib/presentation/recipe/view_model/all_recipe_notifier.dart
@@ -1,22 +1,60 @@
 import 'dart:convert';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:naeng_meh_chu/data/model/recipe_data_response.dart';
 import 'package:naeng_meh_chu/data/repository/fridge_repository.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../../data/model/recipe_data_response.dart';
+final recipeDataResponseProvider = StateNotifierProvider<RecipeNotifier, AsyncValue<List<RecipeDataResponse>>>((ref) {
+  return RecipeNotifier();
+});
 
-part 'all_recipe_notifier.g.dart';
+class RecipeNotifier extends StateNotifier<AsyncValue<List<RecipeDataResponse>>> {
+  RecipeNotifier() : super(const AsyncValue.loading()) {
+    _fetchRecipes();
+  }
 
-@riverpod
-Future<List<RecipeDataResponse>> recipeDataResponse(
-    RecipeDataResponseRef ref) async {
-  final response = await FridgeRepository.allRecipe();
-  final json = jsonDecode(response) as Map<String, dynamic>;
+  int _page = 0;
+  final int _limit = 10;
+  bool _hasNextPage = true;
 
-  final dataList = json['recipeDataResponses'] as List<dynamic>;
+  Future<void> _fetchRecipes({bool isRefresh = false}) async {
+    if (isRefresh) {
+      _page = 0;
+      _hasNextPage = true;
+      state = const AsyncValue.loading();
+    } else {
+      _page++;
+    }
 
-  final List<RecipeDataResponse> models = dataList
-      .map((data) => RecipeDataResponse.fromJson(data as Map<String, dynamic>))
-      .toList();
-  return models;
+    try {
+      final response = await FridgeRepository.allRecipe();
+      final json = jsonDecode(response) as Map<String, dynamic>;
+      final dataList = json['recipeDataResponses'] as List<dynamic>;
+
+      final fetchedRecipes = dataList
+          .skip(_page * _limit)
+          .take(_limit)
+          .map((data) => RecipeDataResponse.fromJson(data as Map<String, dynamic>))
+          .toList();
+
+      if (fetchedRecipes.length < _limit) {
+        _hasNextPage = false;
+      }
+
+      state = AsyncValue.data([
+        if (state.value != null && !isRefresh) ...state.value!,
+        ...fetchedRecipes,
+      ]);
+    } catch (e) {
+    }
+  }
+
+  void fetchMoreRecipes() {
+    if (_hasNextPage) {
+      _fetchRecipes();
+    }
+  }
+
+  void refreshRecipes() {
+    _fetchRecipes(isRefresh: true);
+  }
 }
